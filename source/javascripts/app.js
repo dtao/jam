e76a97337758484455f2fed0b9e5c6ad6c343171
Dan Tao <daniel.tao@gmail.com>
2013-12-02 08:26:15 -0800

    got recording + playback working via crappy code
    needs refactoring!

--- source/javascripts/app.js
+++ source/javascripts/app.js
@@ -6,10 +6,27 @@ window.addEventListener('load', function() {
         ['kick', 'alchemist/AlchemistKick1.wav'],
         ['snare', 'alchemist/AlchemistSnare4.wav'],
         ['hi-hat', 'alchemist/AlchemistHiHat4.wav']
-      ];
+      ],
+      recording = false,
+      startRecording,
+      currentStream = [],
+      playButton = document.getElementById('play'),
+      recordButton = document.getElementById('record');
 
   function displayError(error) {
     alert(error);
+  }
+
+  function showRecording(recording) {
+    if (recording) {
+      playButton.disabled = true;
+      recordButton.disabled = true;
+      recordButton.textContent = 'Recording';
+    } else {
+      playButton.disabled = false;
+      recordButton.disabled = false;
+      recordButton.textContent = 'Record';
+    }
   }
 
   function getAudioContext() {
@@ -39,27 +56,68 @@ window.addEventListener('load', function() {
     });
   }
 
-  function playSound(soundKey) {
+  function playSound(soundKey, start) {
+    if (recording) {
+      currentStream.push({
+        time: context.currentTime - startedRecording,
+        sound: soundKey
+      });
+    }
+
     var source = context.createBufferSource();
     source.buffer = sounds[soundKey];
     source.connect(context.destination);
-    source.start(0);
+    source.start(start || 0);
+  }
+
+  function startPlaying() {
+    if (currentStream.length === 0) {
+      return;
+    }
+
+    var start = context.currentTime;
+    currentStream.forEach(function(sound) {
+      playSound(sound.sound, start + sound.time);
+    });
+  }
+
+  function startRecording() {
+    if (!recording) {
+      recording = true;
+      startedRecording = context.currentTime;
+      currentStream = [];
+
+    } else {
+      recording = false;
+    }
+
+    showRecording(recording);
+  }
+
+  function bindKey(keyCode, callback) {
+    document.addEventListener('keydown', function(e) {
+      if (e.keyCode === keyCode) {
+        callback();
+      }
+    });
+  }
+
+  function bindKeyToSound(keyCode, sound) {
+    bindKey(keyCode, function() {
+      playSound(sound);
+    });
   }
 
   var context = getAudioContext();
 
   loadSoundSamples();
 
-  Mousetrap.bind('space', function() {
-    playSound('kick');
-  });
+  bindKey(82, startRecording);
+  bindKeyToSound(32, 'kick');
+  bindKeyToSound(65, 'snare');
+  bindKeyToSound(16, 'hi-hat');
 
-  Mousetrap.bind('a', function() {
-    playSound('snare');
-  });
-
-  Mousetrap.bind('shift', function() {
-    playSound('hi-hat');
-  });
+  playButton.addEventListener('click', startPlaying);
+  recordButton.addEventListener('click', startRecording);
 
 });
